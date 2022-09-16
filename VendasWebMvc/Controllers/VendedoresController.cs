616c34d8c212c9f847222032e7aba88e1c301230
jonasaacampos <38704271+jonasaacampos@users.noreply.github.com>
2022-09-16 13:54:11 -0300

    Vendedores Controller e link de navegação para departamentos

---
+++ VendasWebMvc/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
